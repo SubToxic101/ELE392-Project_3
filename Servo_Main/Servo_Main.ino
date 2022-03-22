647ab9fc1478663741a805a1dd78a4682c07d914
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2022-03-22 14:16:43 -0400

    working servo

--- Servo_Main/Servo_Main.ino
+++ Servo_Main/Servo_Main.ino
@@ -1,21 +1,26 @@
 #include <Servo.h>
 //int servoPin = A2;
-Servo mainServo;
+Servo servo1;
+Servo servo2;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(A2, OUTPUT);
-  mainServo.attach(A2, 500, 2500);
+  pinMode(A3, OUTPUT);
+  servo1.attach(A2, 500, 2500);
+  servo2.attach(A3, 500, 2500);
   delay(3000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //mainServo.write(0);
-  mainServo.writeMicroseconds(600);
+  servo1.write(0);
+  servo2.write(0);
   delay(1000);
   //mainServo.write(180);
-  mainServo.writeMicroseconds(2400);
+  servo1.write(180);
+  servo2.write(1800);
   delay(1000);
   Serial.println("running");
 }
