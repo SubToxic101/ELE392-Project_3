f00d59291de1c24ed1a8db696172c4d40be791a7
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2022-03-22 13:18:04 -0400

    servo 1

--- Servo_Main/Servo_Main.ino
+++ Servo_Main/Servo_Main.ino
@@ -1,9 +1,18 @@
+#include <Servo.h>
+int servoPin = A2;
+Servo mainServo;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(servoPin, OUTPUT);
+  mainServo.attach(pin, 500, 2500);
+  delay(3000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  servo.write(0);
+  delay(1000);
+  servo.write(180);
+  delay(1000);
 }
