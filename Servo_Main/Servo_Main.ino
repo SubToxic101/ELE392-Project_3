af8adc2451e7a50bda3e55dde5a905467e00ca76
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2022-03-24 14:54:48 -0400

    step response generation

--- Servo_Main/Servo_Main.ino
+++ Servo_Main/Servo_Main.ino
@@ -1,4 +1,4 @@
-#include <Servo.h>
+ #include <Servo.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
@@ -7,8 +7,9 @@ Servo servo1;
 Servo servo2;
 int set_servo = 0;
 int testset = 0;
+int t = 0;
+sensors_event_t event;
 
-#define BNO055_SAMPLERATE_DELAY_MS (100)
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 void setup() {
@@ -18,6 +19,7 @@ void setup() {
   servo1.attach(A2, 500, 2500);
   servo2.attach(A3, 500, 2500);
 
+  servo1.write(0);
   Serial.begin(115200);
   Serial.println("Orientation Sensor Test"); Serial.println("");
   /* Initialise the sensor */
@@ -26,27 +28,30 @@ void setup() {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  delay(1000);
-
-  /* Use external crystal for better accuracy */
   bno.setExtCrystalUse(true);
+  
+  
+  bno.getEvent(&event);
+  
+  delay(3000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly
 
-  
+  /*
    if (Serial.available() > 0) {
     testset = Serial.parseInt();
     Serial.println(testset);
     set_servo = testset;
   }
+  */
   
+  set_servo = 160;
   /* Get a new sensor event */
-  sensors_event_t event;
   bno.getEvent(&event);
-  
-  /* The processing sketch expects data as roll, pitch, heading */
+
+  /* The processing sketch expects data as roll, pitch, heading 
   Serial.print(F("Orientation: "));
   Serial.print((float)event.orientation.x);
   Serial.print(F(" "));
@@ -55,7 +60,7 @@ void loop() {
   Serial.print((float)event.orientation.z);
   Serial.println(F(""));
 
-  /* Also send calibration data for each sensor. */
+   Also send calibration data for each sensor. 
   uint8_t sys, gyro, accel, mag = 0;
   bno.getCalibration(&sys, &gyro, &accel, &mag);
   Serial.print(F("Calibration: "));
@@ -66,8 +71,15 @@ void loop() {
   Serial.print(accel, DEC);
   Serial.print(F(" "));
   Serial.println(mag, DEC);
-
-  delay(BNO055_SAMPLERATE_DELAY_MS);
-
+  */
+  
+  
+  t = millis();
+  Serial.print(t);
+  Serial.print(", ");
+  Serial.print(F(" "));
+  Serial.print((float)event.orientation.y);
+  Serial.println("");
   servo1.write(set_servo);
+  delay(20);
 }
